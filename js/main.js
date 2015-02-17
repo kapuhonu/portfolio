c81773944cf90075000370ea4cabc3741c5b8038
Nathan Phillips <nathan@nathanphillips.net>
2015-02-17 15:29:33 -0500

    JS cleanup and form action.

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ new WOW().init();
 
 $(function() {
 	$('.cd-nav-trigger').click(function(e) {
-		e.preventDefault();
 		$('#cd-main-nav').toggleClass('expanded');
 		$('#cd-nav').toggleClass('expanded');
 		return false;
